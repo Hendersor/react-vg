b387b408fed5c93569f886b43a51e4ad6f6731cf
Francisco <montejof1998@gmail.com>
2022-03-31 10:41:24 -0700

    Design ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./../styles/global.scss";
-
+import { Filter } from "./Filter";
+import { Videogames } from "./Videogames";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Filter />
+      <Videogames />
+    </div>
+  );
 };
 
 export { App };
